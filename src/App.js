d451801b35a948357f0408b042194f557a1a42a0
feelgoon <feelgoon@gmail.com>
2020-09-08 10:41:07 +0900

    0908 prop개념 이해

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+function Food({fav}){
+  return <h1>I like {fav}</h1>
+}
+
+// function Food(props){
+//   {fav} = props;
+//   return <h1>I Like {fav}</h1>
+// }
+
 function App() {
-  return <div>Hello!!!</div>
+  return (
+    <div>
+      <h1>Hello!!!</h1>
+      <Food fav="Kimchi"/>
+      <Food fav="samgiopsal"/>
+      <Food fav="chukumi"/>
+    </div>
+  );
 }
 
 export default App;
